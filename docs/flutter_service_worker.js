6e86634a71815c63c403bd288b5747d14e4c7225
Tommy Le <tommy.le2@gmail.com>
2022-06-13 00:13:00 -0700

    Deploy

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "c3318ffe61c336ad1ce56fa6588f789b",
-"index.html": "dbd5a230086aec552f02355efb5ce78e",
-"/": "dbd5a230086aec552f02355efb5ce78e",
+"index.html": "c2de993621e00c2f8adc517eb1400e66",
+"/": "c2de993621e00c2f8adc517eb1400e66",
 "main.dart.js": "145387bacc6f7351f9f0c8bd51504a61",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
